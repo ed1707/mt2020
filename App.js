e26d4a245fa4b4630f78a224b2dc453db8457e1e
ed1707 <68108208+ed1707@users.noreply.github.com>
2020-08-04 11:29:46 +0200

    Update App.js

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navbar from "./components/navbar.component";
 import UnterbegriffeList from "./components/unterbegriffe-list.component";
+import BegriffeList from "./components/begriffe-list.component";
 import EditUnterbegriff from "./components/edit-unterbegriff.component";
 import CreateUnterbegriff from "./components/create-unterbegriff.component";
 import CreateBegriff from "./components/create-begriff.component";
@@ -15,6 +16,7 @@ function App() {
          <Navbar />
          <br/>
          <Route path="/" exact component={UnterbegriffeList} />
+         <Route path="/" exact component={BegriffeList} />
          <Route path="/edit/:id" component={EditUnterbegriff} />
          <Route path="/create" component={CreateUnterbegriff} />
          <Route path="/begriff" component={CreateBegriff} />

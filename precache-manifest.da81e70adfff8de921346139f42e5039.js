e89a3b95a0c164330d090f04b172ec7c7c2dd524
E D <dieteel@gmail.com>
2020-08-10 16:58:47 +0200

    Updates

---
+++ precache-manifest.da81e70adfff8de921346139f42e5039.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eccabf590ab07d6db73dced2c4aa9f8f",
+    "url": "/kuwerkoapp-new/index.html"
+  },
+  {
+    "revision": "b55549953467dbc4f5ff",
+    "url": "/kuwerkoapp-new/static/css/2.36f319b0.chunk.css"
+  },
+  {
+    "revision": "b55549953467dbc4f5ff",
+    "url": "/kuwerkoapp-new/static/js/2.68882d61.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/kuwerkoapp-new/static/js/2.68882d61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "134f6eecbaa6c528dc62",
+    "url": "/kuwerkoapp-new/static/js/main.0b2d65ab.chunk.js"
+  },
+  {
+    "revision": "99e50e3092b379d0f946",
+    "url": "/kuwerkoapp-new/static/js/runtime-main.952647c5.js"
+  }
+]);
